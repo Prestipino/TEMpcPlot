102cefa61d76164b8dd241a8a512ad78db346927
Carmelo Prestipino <carmelo.prestipino@gmail.com>
2020-04-02 12:18:13 +0200

    docs
    docs

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='TEMpcPlot',
+      version='0.1',
+      description='Tools for ED',
+      url='http://github.com/stopboring/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['TEMpcPlot', 'TEMpcPlot.GII', 'TEMpcPlot.tables',
+                'TEMpcPlot.dm3_lib', 'TEMpcPlot.TEM'],
+      install_requires=['numpy', 'matplotlib', 'scipy'],
+      package_data={'TEMpcPlot.GII': ['libgcc_s_seh-1.dll',
+                                      'libgfortran-3.dll',
+                                      'libgmp-10.dll',
+                                      'libgmpxx-4.dll',
+                                      'libquadmath-0.dll',
+                                      'libwinpthread-1.dll',
+                                      'pydiffax.pyd',
+                                      'pydiffax_36.pyd',
+                                      'pypowder.pyd',
+                                      'pypowder_36.pyd',
+                                      'pyspg.pyd',
+                                      'pyspg_36.pyd'],
+                    'TEMpcPlot.TEM': ['angle.png',
+                                      'down.png',
+                                      'lenght.png',
+                                      'PlotP.png',
+                                      'RemP.png',
+                                      'UP.png']},
+      zip_safe=False)
+
+
