5a6eac91c4047016ba21916a341998a03a5c3067
Lally Carpentier <lallycarpentier@gmail.com>
2021-06-24 09:40:07 +0200

    2 more functions

--- TEMpcPlot/TEM/math_tools.py
+++ TEMpcPlot/TEM/math_tools.py
@@ -46,6 +46,24 @@ def project_v(vect, base):
      as row vectors'''
     return np.abs(vect @ base / mod(base.T)**2)
 
+def change_basis(coor, base):
+    '''chqnge a set of coordinqte verctor in a new base
+    base should be as colums vectors and coor a 2(3..) row coloum
+     as row vectors
+     out a row vector of coordinate'''
+    P = np.linalg.inv(base)  # inverse of column matrix with the base (2*2 square matrix)
+    return  np.dot(P, coor).T # Peaks coordinates in unit cell basis
+ 
+def rest_int(coor, tollerance):
+    """return the coor thqt are ingeger inside a tollerance
+       coor is a row matrix 
+
+    """
+    filt = coor %1 # filter peaks that aren't multiple of the new basis
+    filt = np.where(filt < 0.5, filt , 1-filt) # remainder of euclidean division (<0.5) 
+    return (filt < tollerance).all(axis=1)  # keeps the peaks that can be reindexed into a n*2 boolean array
+
+
 def dist_p2vect(origin, vec, coor):
     """return the distances of a set of point from the line
        the position of the point if given by an iterable of shape 2xN
