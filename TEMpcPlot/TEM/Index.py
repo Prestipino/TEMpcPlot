e6d8f1407831926ae2596be14ddbf781495c617c
carmelo_zbook <carmelo.prestipino@gmail.com>
2021-07-12 17:29:25 +0200

    not woerking

--- TEMpcPlot/TEM/Index.py
+++ TEMpcPlot/TEM/Index.py
@@ -82,8 +82,6 @@ def sort_LayerCalib(Peaks, vects, toll=0.1):
     return np.vstack(bases[argsm])
 
 
-
-
 def Find_2D_uc(Peaks, toll_angle=5, toll=0.10):
     """
     Finds the best fitting unit cell for an image
@@ -189,3 +187,34 @@ def check_3Dlincomb(vectors):
         else:
             break
     return np.array(vect)
+
+
+
+def search_twofold(base, toll_angle):
+    """
+    base are columns vectors for the base
+    """
+    toll = toll_angle * mt.rpd
+
+    rMT = np.linalg.inv(np.dot(base.T, base))
+
+    zz = np.mgrid[-2:3, -2:3, 0:3]
+    uvw = zz.reshape(3, -1).T
+
+    #remove not coprime indices
+    xx =np.gcd.reduce(uvw, axis=1)
+    uvw = uvw[xx== 1]
+
+    # remove collinear vectors
+    UVW = []
+    uvwlen = len(uvw)
+    for i in range(uvwlen):
+        for j in range(i+1, uvwlen):
+            if all(np.cross(uvw[i],uvw[j]) == 0):
+                break
+        else:
+            UVW.append(uvw[i])
+            
+    uvw = np.array(UVW)
+    zz = uvw @ uvw.T
+    couples =np.where((zz == 2)|(zz==1))
